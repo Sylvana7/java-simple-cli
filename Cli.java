a289211c47c6e28563860106903822fd6a062364
Sylvana7 <Sylvana.nirina@laposte.net>
2023-10-20 14:21:17 +0200

    java-simple-cli

--- Cli.java
+++ Cli.java
@@ -3,62 +3,79 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.LocalDateTime;
 import java.util.Properties;
+import java.util.Map;
+import java.io.File;
 
 public class Cli {
 
-           public static void main(String[] args) {
+      public static void main(String[] args) {
            Scanner scanner = new Scanner(System.in);
            System.out.print("> ");
            while (true) {
                    String command = scanner.nextLine();
                    String[] array = command.split(" ", 2);
+		   String[] pathnames = command.split(" ", 0);
                    String output = "";
-                   if (command.equals("exit")) {
+                   if (command.equals("exit") || command.equals("logout")) {
                          break;
                    }
           
                    else if (command.equals("date")) {
-                      LocalDate date = LocalDate.now();
-                      output = date.toString();
-                      
-                    }      
-                 
+                   	LocalDate date = LocalDate.now();
+                   	output = date.toString();    
+                   }
                    else if (command.equals("time")) {
-                      LocalTime time = LocalTime.now();
-                      output = time.toString();
-                     
+                   	LocalTime time = LocalTime.now();
+                   	output = time.toString();              
+                   } 
+                   else if (command.equals("dateTime")) { 
+                   	LocalDateTime dateTime = LocalDateTime.now();
+                   	output = dateTime.toString();
+                   } 
+                   else if (command.equals("useraccount")) {
+                 	output = System.getProperty("user.name");
+                   } 
+                   else if (command.equals("userhome")) {
+                 	output = System.getProperty("user.home");
+                   } 
+                   else if (command.equals("os")) {   
+                	output = System.getProperty("os.name") + " " + System.getProperty("os.version");
 
-                 } else if (command.equals("dateTime")) { 
-                      LocalDateTime dateTime = LocalDateTime.now();
-                      output = dateTime.toString();
-
-             } else if (command.equals("useraccount")) {
-                     output = System.getProperty("user.name");
-
-             } else if (command.equals("userhome")) {
-                     output = System.getProperty("user.home");
-
-         } else if (command.equals("os")) {
+             	   } 
+                   else if (array[0].equals("printenv")) {
+			if (array.length >= 2) {
+			    output = array[1];
+		   } else{
+			 Map<String, String> env = System.getenv();
+                       for (Map.Entry<String, String> entry  : env.entrySet()) { 
+ 			  output += entry.getKey() + "=" + entry.getValue() + System.lineSeparator();
+                   }	
+		   }
                    
-                    output = System.getProperty("os.name") + " " + System.getProperty("os.version");
-
-        } else if (array[0].equals("printenv")) {
-                   if (array.length >= 2) {
-                  output = System.getenv(array[1]);
-                  output = output == null ? "" : output;
-                  }
-       } else if (array[0].equals("echo")) {
-                 if (array.length >= 2) {
-                  output = array[1];
-                  output = output == null ? "" : output;
-                 }
-        } else {
-                  output = "command '" + command + "' not found.";
+             } 
+		   else if (pathnames[0].equals("ls")) {	
+			File f = new File(pathnames[1]);
+			String contents[] = f.list(); 
+			for ( int i = 0; i < pathnames.length; i++) {
+				
+		        	output += contents[i] + System.lineSeparator();           
+				output = output == null ? " Not a directory " : output;
+		   }
+	    
+	    }
+                   else if (array[0].equals("echo") || array[0].equals("print")) {
+                    if (array.length >= 2) {
+                	output = array[1];
+           	    	output = output == null ? "" : output;
             }
-             System.out.println(output);
-             System.out.print("> ");
-         }
-         scanner.close();
-         System.out.println("Bye!");
+                 } else {
+              		output = "command '" + command + "' not found.";
+            }
+
+             	System.out.println(output);
+             	System.out.print("> ");
+            }
+         	scanner.close();
+         	System.out.println("Bye!");
     }
 }
